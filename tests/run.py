df4103e6c05a6f391e5c6c33907c4571d8deed4e
Jeremy Lainé <jeremy.laine@m4x.org>
2012-09-27 00:38:12 +0200

    rework harness

--- tests/run.py
+++ tests/run.py
@@ -11,6 +11,12 @@ for component in ['db', 'http', 'script']:
 os.environ['LD_LIBRARY_PATH'] = ':'.join(path)
 
 # run tests
-for component in ['db', 'http']:
-    prog = os.path.join(os.path.dirname(__file__), component, 'qdjango-%s-tests' % component)
+TESTS = [
+    'db/qdjango-db-tests',
+    'http/qdjangohttpserver/tst_qdjangohttpserver',
+    'http/qdjangourlresolver/tst_qdjangourlresolver',
+    'script/qdjangoscript/tst_qdjangoscript',
+]
+for test in TESTS:
+    prog = os.path.join(os.path.dirname(__file__), test)
     os.system(prog)
