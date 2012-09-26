0cb1e2b10f280f53d17da5910e925ae95e32bd25
Jeremy Lainé <jeremy.laine@m4x.org>
2012-09-27 00:06:51 +0200

    run split tests

--- tests/run.py
+++ tests/run.py
@@ -3,11 +3,14 @@
 import os
 
 root = os.path.join(os.path.dirname(__file__), '..')
-prog = os.path.join(os.path.dirname(__file__), 'qdjango-tests')
 
+# set library path
 path = []
 for component in ['db', 'http', 'script']:
     path.append(os.path.join(root, 'src', component))
-
 os.environ['LD_LIBRARY_PATH'] = ':'.join(path)
-os.system(prog)
+
+# run tests
+for component in ['db', 'http']:
+    prog = os.path.join(os.path.dirname(__file__), component, 'qdjango-%s-tests' % component)
+    os.system(prog)
