e629cadd766726aff32f6b2f706bcf2ea9676205
Jeremy Lainé <jeremy.laine@m4x.org>
2012-09-27 09:50:16 +0200

    make it possible to produce xunit xml output

--- tests/run.py
+++ tests/run.py
@@ -1,10 +1,33 @@
 #!/usr/bin/python
 
+import getopt
 import os
 import platform
+import sys
 
 components = ['db', 'http', 'script']
 root = os.path.dirname(__file__)
+report_path = None
+
+def usage():
+    print "Usage: run.py [options]"
+
+# parse options
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'hx:')
+except getopt.GetoptError, err:
+    print err
+    usage()
+    sys.exit(2)
+
+for opt, optarg in opts:
+    if opt == '-h':
+        usage()
+        sys.exit()
+    elif opt == '-x':
+        report_path = optarg
+        if not os.path.exists(report_path):
+            os.mkdir(report_path)
 
 # set library path
 path = []
@@ -17,12 +40,15 @@ else:
 
 # run tests
 for component in components:
-    component_root = os.path.join(root, component)
-    for test in os.listdir(component_root):
-        test_path = os.path.join(component_root, test)
+    component_path = os.path.join(root, component)
+    for test in os.listdir(component_path):
+        test_path = os.path.join(component_path, test)
         if os.path.isdir(test_path):
             if platform.system() == 'Darwin':
                 prog = os.path.join(test_path, 'tst_' + test + '.app', 'Contents', 'MacOS', 'tst_' + test)
             else:
                 prog = os.path.join(test_path, 'tst_' + test)
-            os.system(prog)
+            if report_path:
+                os.system('%s -xunitxml -o %s/%s.xml' % (prog, report_path, test))
+            else:
+                os.system(prog)
